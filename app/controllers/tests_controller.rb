2cee855a17f436b3ecfff118f3146541779f750d
Tim Garton <garton.tim@gmail.com>
2014-09-23 10:05:33 -0700

    print out cookie info

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,7 +2,11 @@ class TestsController < ApplicationController
   respond_to :json
 
   def test
+    #print informational stuff
+    Rails.logger.info("  Cookies:")
+    cookies.each {|c| Rails.logger.info("    #{c}")}
+
     status = (params[:status_code].nil? ? :ok : params[:status_code].to_i)
-    respond_with({foo: 'bar'}, status: status)
+    respond_with({email: 'jdoe@example.com'}, status: status)
   end
 end
